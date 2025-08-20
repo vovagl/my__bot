f45656eac8fdfc78f603910172dce5f6b0eb74c3
VladimirG-1971 <vova16061971@gmail.com>
2025-08-20 16:36:31 +0300

    20.08

--- api/index.js
+++ api/index.js
@@ -1,46 +1,39 @@
 import { Telegraf, Markup } from "telegraf";
-import dotenv from 'dotenv';
-dotenv.config()
-
-import {getCat} from '../cat.js';
-import { getDog } from "../dog.js";
-import {showMenu} from '../menu.js';
+import { getCat } from '../cat.js';
+import { getDog } from '../dog.js';
+import { showMenu } from '../menu.js';
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.command('start', async ctx =>{
-    await ctx.replyWithHTML (
-        `Привет,  ${ctx.from.first_name || 'незнакомец'}! Если хочешь посмотреть мемы кошек и собак, нажми 'Да'.`,
-        Markup.keyboard([['Да']]).resize()
-    );
+bot.command('start', async ctx => {
+  await ctx.replyWithHTML(
+    `Привет, ${ctx.from.first_name || 'незнакомец'}! Нажми 'Да' чтобы начать.`,
+    Markup.keyboard([['Да']]).resize()
+  );
 });
 
-bot.on("message", async ctx =>{
-  try{
+bot.on("message", async ctx => {
+  try {
     const text = ctx.message.text;
-    if (text === "Да") {
-     await showMenu(bot, ctx.chat.id); 
-    }else if (text === "Получить мем кота") {
-     await ctx.replyWithHTML("...", Markup.removeKeyboard()); 
-    const cat = await getCat();
-    await ctx.reply(cat);
-    await showMenu(bot, ctx.chat.id); 
-  } else if (ctx.message.text === "Получить мем собаки") {
-    await ctx.replyWithHTML("...", Markup.removeKeyboard());
-    const dog = await getDog();
-    await ctx.reply(dog);
-    await showMenu(bot, ctx.chat.id); 
-  }else {
+    if (text === "Да") await showMenu(bot, ctx.chat.id);
+    else if (text === "Получить мем кота") {
+      const cat = await getCat();
+      await ctx.reply(cat);
+      await showMenu(bot, ctx.chat.id);
+    } else if (text === "Получить мем собаки") {
+      const dog = await getDog();
+      await ctx.reply(dog);
+      await showMenu(bot, ctx.chat.id);
+    } else {
       await ctx.reply("Такой команды нет 🙂");
-      await showMenu(bot, ctx.chat.id); 
+      await showMenu(bot, ctx.chat.id);
     }
-  } catch (e) {
-    console.error("Ошибка в обработчике сообщений:", e);
-    await ctx.reply("Произошла ошибка, попробуй позже ❌");
-    await showMenu(bot, ctx.chat.id); 
+  } catch (err) {
+    console.error("Ошибка в обработчике сообщений:", err);
+    await ctx.reply("Произошла ошибка ❌");
+    await showMenu(bot, ctx.chat.id);
   }
-    })
-
+});
 
 export default async function handler(req, res) {
   try {
